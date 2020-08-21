0dc0449749f15bf3c618743d1f8139c930aeff41
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-08-21 09:12:45 +0200

    Updated the local build script.

--- adhesive.sh
+++ adhesive.sh
@@ -4,6 +4,9 @@ echo '##########################################################################
 echo '# adhesive containerized build starting'
 echo '#############################################################################'
 
+# PYTHONUSERSITE disables adding the .local into sys.path, so our $HOME mount
+# doesn't screw up.
+
 docker run -it \
     --rm \
     -v /tmp:/tmp \
@@ -12,8 +15,10 @@ docker run -it \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
     -e HOME=$HOME \
+    -e PYTHONNOUSERSITE=1 \
     -w $(pwd) \
     -u $(id -u):$(id -g) \
     $(id -G | perl -pe 's/(\d+)/--group-add \1/g') \
     germaniumhq/adhesive:1.4.5 \
     adhesive $@
+
